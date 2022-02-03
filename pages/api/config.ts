d04be10ee0d74b7c463aa8e9b738952fc29e1f42
Karim Alibhai <karim@alibhai.co>
2022-02-03 14:23:54 -0500

    add config manager

--- pages/api/config.ts
+++ pages/api/config.ts
@@ -1,19 +1,21 @@
 import * as t from 'io-ts';
 
-import { ConfigProvider } from '../../utils/config';
-import { ApiRequest, routes, createRpcMethod } from '../../utils/http';
+import { ConfigManager } from '../../utils/config';
+import { routes, createRpcMethod } from '../../utils/http';
 
 export const getConfig = createRpcMethod(t.interface({}), async function () {
-    return ConfigProvider.getAll();
-})
+    const config = await ConfigManager.createProvider();
+    return config.getAll();
+});
 
 export const updateConfig = createRpcMethod(t.record(t.string, t.unknown), async function (req) {
+    const config = await ConfigManager.createProvider();
     // any is valid here, the config provider performs validation
-    await ConfigProvider.setAll(req.body as any);
-    return ConfigProvider.getAll();
-})
+    await config.setAll(req.body as any);
+    return config.getAll();
+});
 
 export default routes({
     getConfig,
-    updateConfig,
+    updateConfig
 });
---
+++ utils/config.ts
@@ -0,0 +1,118 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as t from 'io-ts';
+import merge from 'lodash/merge';
+import { validate } from './http';
+
+const ConfigTypes = t.interface({
+    projectPath: t.string,
+    databases: t.record(t.string, t.record(t.string, t.string)),
+    showReactQueryDebugger: t.boolean
+});
+type ConfigTypes = t.TypeOf<typeof ConfigTypes>;
+
+const validateConfig = (config: any) =>
+    validate(
+        ConfigTypes,
+        merge(
+            {
+                projectPath: '',
+                databases: {},
+                showReactQueryDebugger: false
+            },
+            config
+        )
+    );
+
+export class ConfigManager {
+    private readyPromise: Promise<void>;
+    private readyError: Error | null = null;
+    private configData: t.TypeOf<typeof ConfigTypes> | null = null;
+
+    constructor(private readonly configFilePath: string) {
+        this.readyPromise = this.loadConfig();
+    }
+
+    private async waitForReady() {
+        await this.readyPromise;
+        if (this.readyError) {
+            throw this.readyError;
+        }
+    }
+
+    private async loadConfig() {
+        try {
+            const configData = await fs.promises.readFile(this.configFilePath, 'utf8');
+            this.configData = validateConfig(JSON.parse(configData));
+        } catch (error: any) {
+            if (error?.code !== 'ENOENT') {
+                this.readyError = error;
+            } else {
+                this.configData = validateConfig({});
+            }
+        }
+
+        Object.assign(this.configData, { __filename: this.configFilePath });
+    }
+
+    private async flushConfig() {
+        // storing as human readable
+        await fs.promises.writeFile(this.configFilePath, JSON.stringify(this.configData, null, '\t'));
+    }
+
+    async setValue<K extends keyof ConfigTypes>(key: K, value: ConfigTypes[K]): Promise<void> {
+        await this.waitForReady();
+        this.configData = validateConfig({
+            ...this.configData,
+            [key]: value
+        });
+        await this.flushConfig();
+    }
+
+    async getValue<K extends keyof ConfigTypes>(key: K): Promise<ConfigTypes[K]> {
+        await this.waitForReady();
+        return validateConfig(this.configData)[key];
+    }
+
+    async getAll() {
+        await this.waitForReady();
+        return this.configData!;
+    }
+
+    async setAll(updates: t.TypeOf<typeof ConfigTypes>) {
+        await this.waitForReady();
+        this.configData = validateConfig(updates);
+        await this.flushConfig();
+    }
+
+    static async createProvider() {
+        try {
+            await fs.promises.stat(path.resolve(process.cwd(), '.git'));
+        } catch (error: any) {
+            if (error.code === 'ENOENT') {
+                throw new Error(`Please run sidekick from the root of your project (.git was not found)`);
+            }
+        }
+
+        try {
+            const { name } = JSON.parse(
+                await fs.promises.readFile(path.resolve(process.cwd(), 'package.json'), 'utf8')
+            );
+
+            const home = process.env.HOME;
+            if (!home) {
+                throw new Error(`Whoa - no home directory specified. What sorcery is this.`);
+            }
+
+            const configDirectory = path.resolve(home, '.sidekick', name.replace(/[\W]+/g, '_'));
+
+            await fs.promises.mkdir(configDirectory, {
+                recursive: true
+            });
+
+            return new ConfigManager(path.resolve(configDirectory, 'config.json'));
+        } catch (error: any) {
+            throw new Error(`Failed to load package.json: ${error.message || error}`);
+        }
+    }
+}

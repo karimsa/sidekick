2144f07baa95fe520719b018cb3dda357bbbe3c3
Karim Alibhai <karim@alibhai.co>
2022-02-03 12:57:47 -0500

    add ts

---
+++ pages/_app.tsx
@@ -0,0 +1,49 @@
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Toaster } from 'react-hot-toast';
+import Head from 'next/head';
+
+import '../styles/globals.scss';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: true
+        }
+    }
+});
+
+function QueryDevtools() {
+    const { data: config } = useRequest<Record<string, any>>('/api/config', {});
+    if (!config?.showReactQueryDebugger) {
+        return null;
+    }
+    return <ReactQueryDevtools />;
+}
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <link
+                    rel={'icon'}
+                    href={
+                        'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡️</text></svg>'
+                    }
+                />
+                <link
+                    rel={'stylesheet'}
+                    type={'text/css'}
+                    data-name={'vs/editor/editor.main'}
+                    href={'https://cdn.jsdelivr.net/npm/monaco-editor@0.25.2/min/vs/editor/editor.main.css'}
+                />
+            </Head>
+            <QueryClientProvider client={queryClient}>
+                <Toaster />
+                <QueryDevtools />
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </>
+    );
+}

55b3694daa8bc30f893ff7f61f1fffd3bedfe0c1
Karim Alibhai <karim@alibhai.co>
2022-02-06 19:47:10 -0500

    add route change toasts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,52 @@
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import Head from 'next/head';
+import { Router } from 'next/router';
 
 import '../styles/globals.scss';
 import 'tippy.js/dist/tippy.css';
 import { useRpcQuery } from '../hooks/useQuery';
 import { getConfig } from './api/config';
+
+/**
+ * Some toast decoration around next.js' async routing.
+ */
+Router.events.on('routeChangeStart', (_, { shallow }) => {
+    if (!shallow) {
+        const toastId = 'route-change';
+        let done = false;
+        const timer = setTimeout(() => {
+            if (!done) {
+                toast.loading(`Loading ...`, { id: toastId });
+            }
+        }, 100);
+        const onSuccess = () => {
+            unload();
+            if (toastId) {
+                toast.dismiss(toastId);
+            }
+        };
+        const onError = err => {
+            unload();
+            if (err.cancelled) {
+                toast.dismiss(toastId);
+            } else {
+                toast.error(`Failed to load new page.`, { id: toastId });
+            }
+        };
+        const unload = () => {
+            done = true;
+            clearTimeout(timer);
+            Router.events.off('routeChangeComplete', onSuccess);
+            Router.events.off('routeChangeError', onError);
+        };
+
+        Router.events.on('routeChangeComplete', onSuccess);
+        Router.events.on('routeChangeError', onError);
+    }
+});
 
 const queryClient = new QueryClient({
     defaultOptions: {

4a1650de1e8d959d7fcfcf20acc7dc1a9d35b63f
Karim Alibhai <karim@alibhai.co>
2022-05-04 00:16:45 -0400

    fix types

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,38 +1,40 @@
 import classNames from 'classnames';
-import { forwardRef } from 'react';
+import { forwardRef, KeyboardEvent } from 'react';
 
 interface InputProps {
 	type?: 'text' | 'number';
 	className?: string;
 	value: string;
 	onChange(value: string): void;
-	onKeyDown?: (evt: KeyboardEvent) => void;
+	onKeyDown?: (evt: KeyboardEvent<HTMLInputElement>) => void;
 	placeholder?: string;
 	autoFocus?: boolean;
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
-	{
-		type = 'text',
-		className,
-		value,
-		onChange,
-		onKeyDown,
-		placeholder,
-		autoFocus,
+export const Input = forwardRef<HTMLInputElement | null, InputProps>(
+	function Input(
+		{
+			type = 'text',
+			className,
+			value,
+			onChange,
+			onKeyDown,
+			placeholder,
+			autoFocus,
+		},
+		ref,
+	) {
+		return (
+			<input
+				ref={ref}
+				className={classNames(className, 'rounded p-3 bg-white border')}
+				type={type}
+				value={value}
+				onChange={(evt) => onChange(evt.target.value)}
+				onKeyDown={onKeyDown}
+				placeholder={placeholder}
+				autoFocus={autoFocus}
+			/>
+		);
 	},
-	ref,
-) {
-	return (
-		<input
-			ref={ref}
-			className={classNames(className, 'rounded p-3 bg-white border')}
-			type={type}
-			value={value}
-			onChange={(evt) => onChange(evt.target.value)}
-			onKeyDown={onKeyDown}
-			placeholder={placeholder}
-			autoFocus={autoFocus}
-		/>
-	);
-});
+);

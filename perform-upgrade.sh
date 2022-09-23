e664d2938d141452954a296ae2989d14dec41e97
Karim Alibhai <karim@alibhai.co>
2022-09-23 01:54:48 -0400

    fix upgrade retries

--- perform-upgrade.sh
+++ perform-upgrade.sh
@@ -5,12 +5,12 @@ TARGET="$1"
 
 # get rid of local branch, in case of rebases
 git reset --hard HEAD
-git checkout -b tmp
-git branch -D $TARGET
+git checkout -b tmp || true
+git branch -D $TARGET || true
 
 # fetch updated refs
 git fetch --all --prune
 
 # create new local branch from remote
 git checkout $TARGET
-git branch -D tmp
+git branch -D tmp || true

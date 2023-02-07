96ad3ff997d34a0871a7668416af3385b4734805
Karim Alibhai <karim@alibhai.co>
2023-02-07 13:55:09 -0500

    chore: some test improvements (#112)
    (cherry picked from commit ca3b8f840c7a2c486c3aba647ca975fa55f1a803)

--- server/cli/__tests__/basic-cli.spec.ts
+++ server/cli/__tests__/basic-cli.spec.ts
@@ -1,6 +1,6 @@
 import { afterEach, describe, expect, it } from '@jest/globals';
 import * as path from 'path';
-import { buildFs, runCliForTesting, TestCleanup } from './test-utils';
+import { buildFs, TestCleanup, runCliForTesting } from './test-utils';
 
 describe('sidekick version', () => {
 	const cleanup = new TestCleanup();
@@ -15,15 +15,11 @@ describe('sidekick version', () => {
 				version: '1.0.0',
 				workspaces: ['./packages/*'],
 			}),
-			'./node_modules/@karimsa/sidekick/package.json': JSON.stringify({
-				name: '@karimsa/sidekick',
-				version: '0.0.0',
-			}),
+			'./packages/foo/src/index.ts': 'export const a = 1',
 		});
 		cleanup.push(targetDir.cleanup);
 
 		const env = {
-			PROJECT_PATH: targetDir.path,
 			PATH: `${process.env.PATH}:${path.resolve(
 				process.cwd(),
 				'node_modules',
@@ -31,7 +27,40 @@ describe('sidekick version', () => {
 			)}`,
 		};
 
-		const result = await runCliForTesting('yarn cli version', env);
-		expect(result.exitCode).toEqual(0);
+		// Should work with 'PROJECT_PATH' set
+		{
+			const result = await runCliForTesting('version', {
+				env: {
+					...env,
+					PROJECT_PATH: targetDir.path,
+				} as any,
+			});
+			expect(result.exitCode).toEqual(0);
+		}
+
+		// Should fail without project root
+		await expect(
+			runCliForTesting('version', {
+				env: env as any,
+			}),
+		).rejects.toThrow(/Could not find sidekick.config.ts/);
+
+		// Should work from project root
+		{
+			const result = await runCliForTesting('version', {
+				env: env as any,
+				cwd: targetDir.path,
+			});
+			expect(result.exitCode).toEqual(0);
+		}
+
+		// Should work from project root's sub directory
+		{
+			const result = await runCliForTesting('version', {
+				env: env as any,
+				cwd: path.resolve(targetDir.path, 'packages/foo'),
+			});
+			expect(result.exitCode).toEqual(0);
+		}
 	}, 10e3);
 });
